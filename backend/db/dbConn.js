f0c76a5c4f47ca7e206ebf85e515b534857cf236
adityakmishra <adityamishra1872@gmail.com>
2024-11-11 19:52:35 +0530

    Resolved merge conflict in dbConn.js

--- backend/db/dbConn.js
+++ backend/db/dbConn.js
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
-require("dotenv").config()
+require("dotenv").config();
 
-const mongoose = require('mongoose')
-
-const connectDB = async()=>{
-    try{
-       await mongoose.connect(process.env.MONGODB_URL)
-        console.log('Connected to DataBase')
-    }
-    catch(err){
-        console.log(err)
-=======
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
@@ -23,7 +12,6 @@ const connectDB = async () => {
     } catch (error) {
         console.error('Database connection error:', error);
         process.exit(1); // Exit process with failure
->>>>>>> 045065fceb086462963a71fe26a94bd98ab1f02f
     }
 };
 
