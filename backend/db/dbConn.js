aafbe2d1a6c7b36b60ed310bf36a53222bb97087
adityakmishra <adityamishra1872@gmail.com>
2024-11-11 20:15:38 +0530

    first commit

--- backend/db/dbConn.js
+++ backend/db/dbConn.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
+        await mongoose.connect(process.env.MONGODB_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
