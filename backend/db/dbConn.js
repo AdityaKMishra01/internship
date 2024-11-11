ba2a23e66eae4bd721c66b62b92c4ac881aaf627
adityakmishra <adityamishra1872@gmail.com>
2024-11-11 22:56:39 +0530

    commit

--- backend/db/dbConn.js
+++ backend/db/dbConn.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 
 const mongoose = require('mongoose');
+url = "mongodb+srv://adityamishra1872:adi1111@cluster0.5subt.mongodb.net/internshipassignment?retryWrites=true&w=majority&appName=Cluster0";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL);
+        await mongoose.connect(url);
         console.log('Connected to Database');
     } catch (error) {
         console.error('Database connection error:', error);
