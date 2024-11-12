5ee73e2099a4bdcf91229f7d5350252bab3e04cf
adityakmishra <adityamishra1872@gmail.com>
2024-11-12 21:19:34 +0530

    commit

--- backend/db/dbConn.js
+++ backend/db/dbConn.js
@@ -1,15 +1,16 @@
 require("dotenv").config();
-
 const mongoose = require('mongoose');
-url = "mongodb+srv://adityamishra1872:adi1111@cluster0.5subt.mongodb.net/internshipassignment?retryWrites=true&w=majority&appName=Cluster0";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(url);
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log('Connected to Database');
     } catch (error) {
         console.error('Database connection error:', error);
-        process.exit(1); // Exit process with failure
+        process.exit(1);
     }
 };
 
