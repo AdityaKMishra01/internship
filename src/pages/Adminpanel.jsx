de7917e6c6d7adec41f10320a1bd7a8cedfa70bb
adityakmishra <adityamishra1872@gmail.com>
2024-11-14 09:40:52 +0530

    commit

--- src/pages/Adminpanel.jsx
+++ src/pages/Adminpanel.jsx
@@ -39,7 +39,6 @@ const AdminPanel = () => {
   const fetchUsers = async () => {
     try {
         const response = await axios.get(`${url}/api/users`);
-        console.log(response.data); // Verify the response structure
         setUsers(Array.isArray(response.data) ? response.data : []);
     } catch (error) {
         console.error("Error fetching users:", error);
